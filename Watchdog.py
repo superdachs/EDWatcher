5c24eb0704833a06879af97c466b2939134abd0a
Stefan Kauerauf <mail@stefankauerauf.de>
2018-01-14 03:29:43 +0100

    removed old app

---
+++ Watchdog.py
@@ -0,0 +1,16 @@
+
+class DirectoryWatchdog:
+    def __init__(self):
+        pass
+
+class FileWatchdog:
+# TODO: https://stackoverflow.com/questions/5419888/reading-from-a-frequently-updated-file
+    def __init__(self):
+        pass
+
+
+class Watchdog:
+    def __init__(self, directory, config):
+        self.directory = directory
+        self.conf = config
+
